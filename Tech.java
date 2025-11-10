5032b4f48aded123f6f0d10069fc6f66fb0a7be9
Alex <ahaarbrink497@g.fhsdschools.org>
2025-11-10 10:22:58 -0600

    made constructors for tech

--- Tech.java
+++ Tech.java
@@ -4,5 +4,17 @@ public class Tech {
     private int yearMade;
     private double price;
 
+    public Tech() {
+        this.color = "gray";
+        this.brand = "apple";
+        this.yearMade = 0;
+        this.price = 0.0;
+    }
+    public Tech(String color, String brand, int yearMade, double price) {
+        this.color = color;
+        this.brand = brand;
+        this.yearMade = yearMade;
+        this.price = price;
+    }
     
 }
