60fe776d8c60d97f65c31ee4b9dffaecd89267fd
Vrishank <vadivrishank@gmail.com>
2025-11-10 11:02:50 -0600

    added device getters and setters

--- Device.java
+++ Device.java
@@ -1,12 +1,22 @@
 public class Device extends Tech {
     private String deviceType;
+    private boolean hasCellular;
 
     public Device() {
         this.deviceType = "";
     }
 
+    public Device() {
+        this.hasCellular = "False";
+    }
+
+
     public Device(String deviceType) {
         this.deviceType = deviceType;
+    }
+
+    public Device(Boolean hasCellular) {
+        this.hasCellular = hasCellular;
     }
     
     public String getDeviceType() {
@@ -15,5 +25,13 @@ public class Device extends Tech {
 
     public void setDeviceType(String newDeviceType) {
         this.deviceType = newDeviceType;
+    }
+
+    public Boolean getHasCellular() {
+        return hasCellular;
+    }
+
+    public void setHasCellular(Boolean newHasCellular) {
+        this.hasCellular = newHasCellular;
     }
 }
