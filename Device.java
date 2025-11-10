2c673abb60937b31f1fe44f8abf1d26a2bf9caa6
Vrishank <vadivrishank@gmail.com>
2025-11-10 10:55:47 -0600

    worked on device

--- Device.java
+++ Device.java
@@ -1,4 +1,19 @@
 public class Device extends Tech {
     private String deviceType;
+
+    public Device() {
+        this.deviceType = "";
+    }
+
+    public Device(String deviceType) {
+        this.deviceType = deviceType;
+    }
     
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String newDeviceType) {
+        this.deviceType = newDeviceType;
+    }
 }
