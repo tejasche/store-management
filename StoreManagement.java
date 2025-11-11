00de85259810b6e5fb7b752e4a4d09b1b05f66f0
Alex <ahaarbrink497@g.fhsdschools.org>
2025-11-11 10:42:57 -0600

    added command line thingy

--- StoreManagement.java
+++ StoreManagement.java
@@ -1,7 +1,11 @@
+
+import java.util.Scanner;
+
 public class StoreManagement {
     public static void main(String[] args){
         
         Peripheral peripheral = new Peripheral();
         System.out.println(peripheral);
+    
     }  
 }
