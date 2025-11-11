513a25e5ba7a25acbe265829ac1ee77f1429058d
Tejas Cherukuru <cherukurutejas@gmail.com>
2025-11-11 13:17:42 -0600

    Merge remote-tracking branch 'origin/main' fix yearMade variable
    # Conflicts:
    #	Tech.java

---
+++ CommandLine.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class CommandLine {
+    public void runCommandLine(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("1: Add an Object\n2: List an Object\n3: Delete an Object");
+        int userDesire = input.nextInt();
+
+        if (userDesire == 1) {
+            //Add object code
+            System.out.println("which object would you like to add?");
+            String addedObject = input.next();
+            LISTNAME.add(addedObject);
+        }
+        if (userDesire == 2) {
+            //list object code
+        }
+        if (userDesire == 3) {
+            System.out.println("What object do you want to delet? ");
+            String deletedObject = input.next();
+            LISTNAME.delete(deletedObject);
+        }
+    }
+}
