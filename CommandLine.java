823cda4afaac531d832482352051a88dc0e47396
Tejas Cherukuru <cherukurutejas@gmail.com>
2025-11-11 13:23:26 -0600

    merged and fixed your guys' shit code

--- CommandLine.java
+++ CommandLine.java
@@ -1,25 +1,36 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CommandLine {
     public void runCommandLine(){
+        ArrayList<String> objects = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         System.out.println("1: Add an Object\n2: List an Object\n3: Delete an Object");
-        int userDesire = input.nextInt();
+        while (true){
+            int userChoice = input.nextInt();
 
-        if (userDesire == 1) {
-            //Add object code
-            System.out.println("which object would you like to add?");
-            String addedObject = input.next();
-            LISTNAME.add(addedObject);
-        }
-        if (userDesire == 2) {
-            //list object code
-        }
-        if (userDesire == 3) {
-            System.out.println("What object do you want to delet? ");
-            String deletedObject = input.next();
-            LISTNAME.delete(deletedObject);
+            if (userChoice == 1) {
+                //Add object code
+                System.out.println("which object would you like to add?");
+                String addedObject = input.next();
+                LISTNAME.add(addedObject);
+            }
+            else if (userChoice == 2) {
+                //list object code
+            }
+            else if (userChoice == 3) {
+                System.out.println("What object do you want to delet? ");
+                String deletedObject = input.next();
+                LISTNAME.delete(deletedObject);
+            }
+
+            else if (userChoice == 4) {
+                break;
+            }
+            else{
+                System.out.println("Invalid choice. Try again.");
+            }
         }
     }
 }
