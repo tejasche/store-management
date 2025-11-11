ef6517e5e08dc01841e5e44cddcd406d381c83fd
Vrishank <avadi865@g.fhsdschools.org>
2025-11-11 16:25:19 -0600

    kinda finished while loop

--- Commandline.java
+++ Commandline.java
@@ -7,8 +7,13 @@ public class Commandline {
     public void commandLine() {
         Scanner input = new Scanner(System.in);
         boolean running = true;
-            while(running) {
-                System.out.println("Device manager");
+            while(running) {  
+                System.out.println("\n=== Device Manager ===");
+                System.out.println("1. Add new device");
+                System.out.println("2. List all devices");
+                System.out.println("3. Delete a device");
+                System.out.println("4. Exit");
+                System.out.print("Enter choice: ");
             }
        
     }
