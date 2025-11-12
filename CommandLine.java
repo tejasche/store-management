eeb5ea50094cbf7648c602be3cfd11f2a1e763ab
Tejas Cherukuru <cherukurutejas@gmail.com>
2025-11-12 10:56:10 -0600

    shit ton of work on case 1

--- CommandLine.java
+++ CommandLine.java
@@ -10,11 +10,33 @@ public class CommandLine {
         while (true){
             System.out.println("\n1: Add an Object\n2: List an Object\n3: Delete an Object\n4: Quit");
             int userChoice = input.nextInt();
+
             if (userChoice == 1) {
                 //Add object code
-                System.out.println("which object would you like to add?");
-                String addedObject = input.next();
-                objects.add(addedObject);
+                System.out.println("Would you like to create a peripheral or a device? ");
+                String whichObject = input.next();
+                whichObject.strip();
+                whichObject.toLowerCase();
+
+                System.out.println("What color is this device? ");
+                String color = input.next();
+
+                System.out.println("What brand is this device? ");
+                String brand = input.next();
+
+                System.out.println("What year was this device made in? ");
+                int year = input.nextInt();
+
+                System.out.println("What is the price of this device? ");
+                double price = input.nextDouble();
+
+                if (whichObject.equals("peripheral")){
+                    System.out.println("What is the device type? ");
+                    String deviceType = input.next();
+
+                    System.out.println("Does this device have cellular? ");
+                    Boolean hasCellular = input.nextBoolean();
+                }
             }
             else if (userChoice == 2) {
                 //list object code
