5836a863ae821e835e5d89025e5711e73bd351d4
Tejas Cherukuru <cherukurutejas@gmail.com>
2025-11-11 18:57:12 -0600

    basic tweaks to CommandLine

--- CommandLine.java
+++ CommandLine.java
@@ -6,32 +6,29 @@ public class CommandLine {
     public void runCommandLine(){
         ArrayList<Object> objects = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        System.out.println("1: Add an Object\n2: List an Object\n3: Delete an Object\n4: Quit");
-        while (true){
-            int userChoice = input.nextInt();
 
+        while (true){
+            System.out.println("\n1: Add an Object\n2: List an Object\n3: Delete an Object\n4: Quit");
+            int userChoice = input.nextInt();
             if (userChoice == 1) {
                 //Add object code
                 System.out.println("which object would you like to add?");
                 String addedObject = input.next();
                 objects.add(addedObject);
             }
-
             else if (userChoice == 2) {
                 //list object code
             }
-
             else if (userChoice == 3) {
-                System.out.println("What object do you want to delet? ");
+                System.out.println("What object do you want to delete? ");
                 String deletedObject = input.next();
-                objects.delete(deletedObject);
+                objects.remove(deletedObject);
             }
-
             else if (userChoice == 4) {
+                System.out.println("Bye!");
                 break;
             }
-
-            else{
+            else {
                 System.out.println("Invalid choice. Try again.");
             }
         }
